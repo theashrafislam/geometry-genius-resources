11eef52dc8fa561bd0bfa78fccc011736a260964
Ashraful Islam <155339396+theashrafislam@users.noreply.github.com>
2024-02-14 10:34:57 +0600

    Overall website is Complate

--- scripts/pentagon-calculate.js
+++ scripts/pentagon-calculate.js
@@ -1,14 +1,33 @@
-function pentagonCalculate(){
-    const pentagonOneInput = document.getElementById('pentagon-height');
-    const pentagonOneValue = pentagonOneInput.value;
-    const one = parseFloat(pentagonOneValue);
+// function pentagonCalculate(){
+//     const pentagonOneInput = document.getElementById('pentagon-height');
+//     const pentagonOneValue = pentagonOneInput.value;
+//     const one = parseFloat(pentagonOneValue);
 
-    const pentagonTwoInput = document.getElementById('pentagon-base');
-    const pentagonTwoValue = pentagonTwoInput.value;
-    const two = parseFloat(pentagonTwoValue);
+//     const pentagonTwoInput = document.getElementById('pentagon-base');
+//     const pentagonTwoValue = pentagonTwoInput.value;
+//     const two = parseFloat(pentagonTwoValue);
 
-    const area = 0.5 * one * two;
+//     const area = 0.5 * one * two;
     
-    const pentagonAreaSpan = document.getElementById('pentagon-area');
-    pentagonAreaSpan.innerText = area;
+//     const pentagonAreaSpan = document.getElementById('pentagon-area');
+//     pentagonAreaSpan.innerText = area;
+// }
+
+
+//another way
+function calculatePentagonArea(){
+    const perimeter = getInputValueById('pentagon-height');
+    const perimeter2 = getInputValueById('pentagon-base');
+    const area = 0.5 * perimeter * perimeter2;
+    setAreaValue('pentagon-area', area);
+}
+function getInputValueById(inputFieldId){
+    const inputField = document.getElementById(inputFieldId);
+    const inputValueText = inputField.value;
+    const value = parseFloat(inputValueText);
+    return value;
+}
+function setAreaValue(elementId, area){
+    const element = document.getElementById(elementId);
+    element.innerText = area;
 }
